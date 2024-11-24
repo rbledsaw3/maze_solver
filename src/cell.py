2383da188202c6ff35f788f5f30e53d3b7debc24
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-11-23 20:10:23 -0500

    create draw_move() method for cells to show path between two cells going from center-to-center

--- src/cell.py
+++ src/cell.py
@@ -13,6 +13,8 @@ class Cell:
         self.win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self.win is None:
+            return
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
@@ -29,3 +31,19 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half_length1 = abs(self.x2 - self.x1) // 2
+        x_center = self.x1 + half_length1
+        y_center = self.y1 + half_length1
+
+        half_length2 = abs(to_cell.x2 - to_cell.x1) // 2
+        x_center2 = to_cell.x1 + half_length2
+        y_center2 = to_cell.y1 + half_length2
+
+        fill_color= "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self.win.draw_line(line, fill_color)
--- src/main.py
+++ src/main.py
@@ -5,20 +5,28 @@ def main():
     win = Window(800, 600)
 
     c_0 = Cell(win)
-    c_0.has_left_wall = False
+    c_0.has_right_wall = False
     c_0.draw(50, 50, 100, 100)
 
     c_1 = Cell(win)
-    c_1.has_right_wall = False
-    c_1.draw(125, 125, 200, 200)
-
-    c_1 = Cell(win)
+    c_1.has_left_wall = False
     c_1.has_bottom_wall = False
-    c_1.draw(255, 255, 250, 250)
+    c_1.draw(100, 50, 150, 100)
+
+    c_0.draw_move(c_1)
+
+    c_2 = Cell(win)
+    c_2.has_top_wall = False
+    c_2.has_right_wall = False
+    c_2.draw(100, 100, 150, 150)
+
+    c_1.draw_move(c_2)
 
     c_3 = Cell(win)
-    c_3.has_top_wall = False
-    c_3.draw(300, 300, 500, 500)
+    c_3.has_left_wall = False
+    c_3.draw(150, 100, 200, 150)
+
+    c_2.draw_move(c_3, True)
 
     win.wait_for_close()
 
