4749186deb555ca46b7585e3258bcfa97b1a4528
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-11-23 19:48:37 -0500

    added Point and Line classes. renamed window.py to graphics.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from window import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(100, 100), Point(200, 200))
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
