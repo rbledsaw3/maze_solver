c97b7572fae34e34fa34fe85bb7640d35feecc02
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-11-23 20:19:15 -0500

    created Maze class including animation of maze building

--- src/main.py
+++ src/main.py
@@ -1,33 +1,21 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 600)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) // num_cols
+    cell_size_y = (screen_y - 2 * margin) // num_rows
+    win = Window(screen_x, screen_y)
 
-    c_0 = Cell(win)
-    c_0.has_right_wall = False
-    c_0.draw(50, 50, 100, 100)
-
-    c_1 = Cell(win)
-    c_1.has_left_wall = False
-    c_1.has_bottom_wall = False
-    c_1.draw(100, 50, 150, 100)
-
-    c_0.draw_move(c_1)
-
-    c_2 = Cell(win)
-    c_2.has_top_wall = False
-    c_2.has_right_wall = False
-    c_2.draw(100, 100, 150, 150)
-
-    c_1.draw_move(c_2)
-
-    c_3 = Cell(win)
-    c_3.has_left_wall = False
-    c_3.draw(150, 100, 200, 150)
-
-    c_2.draw_move(c_3, True)
-
+    maze = Maze(
+            margin, margin, num_rows, num_cols,
+            cell_size_x, cell_size_y, win
+            )
+    
     win.wait_for_close()
 
 main()
